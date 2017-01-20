0a4019c4d64d1056a5f4597576ed29501c4657d2
vk hooda <er.vkhooda24@gmail.com>
2017-01-20 00:03:14 -0500

    dummy models created

--- todosapp/models.py
+++ todosapp/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class NotesList(models.Model):
+	title = models.CharField(max_length = 30)
+	desc  = models.CharField(max_length = 30)
+	author= models.CharField(max_length =30)
